58aa41d60f4d80f1cb58b7baa2c36ed46c15d55b
MayankRohilla17299 <52850405+MayankRohilla17299@users.noreply.github.com>
2025-06-13 10:30:41 +0530

    Bug fix

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -11,7 +11,7 @@ function CartOverview() {
         <div className="bg-stone-800 text-stone-200 uppercase p-4 sm:p-6 text-sm md:text-base flex items-center justify-between">
             <p className="text-stone-300 font-semibold space-x-4 sm:space-x-6">
                 <span>{numPizzas} pizzas</span>
-                <span>${totalCost}</span>
+                <span>₹{totalCost}</span>
             </p>
             <Link to="/cart">Open cart &rarr;</Link>
         </div>
